4d1c0a29e3b6e1b2f0852630a87bf30e2245e47f
aaru01 <aartimedhe16601@gmail.com>
2023-09-24 21:59:24 -0700

    added contact app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './views/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>hii</h1>
+ <App />
 );
 
---
+++ src/views/App/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import  phonebook from './../../data/phonebook'
+import ContactCard from './../../components/ContactCard/ContactCard';
+
+function App() {
+    const [contacts, setContacts] = useState(phonebook);
+    const [searchTerm, setSearchTerm] = useState('');
+
+useEffect(()=>{
+ const filteredContacts =  phonebook.filter((contact)=>{
+const name = contact.name.toLowerCase();
+const mobile = contact.mobile.toString();
+const query = searchTerm.toLowerCase();
+
+return (name.includes(query) || mobile.includes(query))
+})
+setContacts(filteredContacts);
+
+},[searchTerm])
+
+    return (
+        <>
+    <h1 className='text-center'>Contact Baba</h1>
+ <input type='text' 
+ placeholder='Search' 
+ className='search' 
+ value={searchTerm}
+ onChange={(e)=>{setSearchTerm(e.target.value)}}
+ />
+ 
+ <div>
+    {contacts.map((contact, index)=>{
+     const {name, mobile} = contact;
+    
+    return <ContactCard key={index} name ={name} mobile={mobile} />
+    })}
+     {
+    contacts.length === 0 ? <h2 className='text-center'>No Contacts Found</h2>: null
+ }
+ </div>
+
+ </>
+)
+}
+
+export default App
